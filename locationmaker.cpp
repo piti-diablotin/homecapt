b367d0bdc93a109587cdc695bdc0fa9e87822ccd
Jordan Bieder <jordan.bieder@geduld.fr>
2019-05-30 13:07:49 +0200

    Create location

---
+++ locationmaker.cpp
@@ -0,0 +1,45 @@
+#include "locationmaker.h"
+#include "ui_locationmaker.h"
+
+LocationMaker::LocationMaker(HomeCaptAPI *api, QWidget *parent) :
+  QDialog(parent),
+  ui(new Ui::LocationMaker),
+  _api(api)
+{
+  ui->setupUi(this);
+  QObject::connect(_api,SIGNAL(errorJson(QString)),this,SLOT(displayError(QString)));
+  QObject::connect(_api,SIGNAL(errorReply(QString)),this,SLOT(displayError(QString)));
+  QObject::connect(_api,SIGNAL(locationCreated()),this,SLOT(accept()));
+}
+
+LocationMaker::~LocationMaker()
+{
+  delete ui;
+}
+
+void LocationMaker::on_buttonBox_accepted()
+{
+  if (ui->name->text().isEmpty())
+  {
+    ui->info->setText(tr("Please provide a name"));
+    return;
+  }
+  if (_api != 0 && _api->isReady())
+  {
+    _api->createLocation(ui->name->text().toUtf8());
+  }
+  else {
+    ui->info->setText(tr("API not ready"));
+  }
+}
+
+void LocationMaker::displayError(QString msg)
+{
+  ui->info->setText(msg);
+}
+
+void LocationMaker::accept()
+{
+  QDialog::done(QDialog::Accepted);
+  this->close();
+}
---
+++ locationmaker.h
@@ -0,0 +1,29 @@
+#ifndef LOCATIONMAKER_H
+#define LOCATIONMAKER_H
+
+#include <QDialog>
+#include "homecaptapi.h"
+
+namespace Ui {
+  class LocationMaker;
+}
+
+class LocationMaker : public QDialog
+{
+    Q_OBJECT
+
+  public:
+    explicit LocationMaker(HomeCaptAPI *api = 0, QWidget *parent = 0);
+    ~LocationMaker();
+
+  private slots:
+    void on_buttonBox_accepted();
+    void displayError(QString msg);
+    void accept();
+
+  private:
+    Ui::LocationMaker *ui;
+    HomeCaptAPI *_api;
+};
+
+#endif // LOCATIONMAKER_H
